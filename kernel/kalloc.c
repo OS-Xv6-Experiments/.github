1b4d390ee9b60f70ff3ae8954e1418bcc841db66
WZK040104 <2762822216@qq.com>
2024-07-18 13:48:04 +0800

    xv6 实验6 thread

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,49 +9,34 @@
 #include "riscv.h"
 #include "defs.h"
 
-void freerange(void* pa_start, void* pa_end);
+void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-// defined by kernel.ld.
+                   // defined by kernel.ld.
 
 struct run {
-  struct run* next;
+  struct run *next;
 };
 
-struct kernelmem
-{
+struct {
   struct spinlock lock;
-  struct run* freelist;
+  struct run *freelist;
 } kmem;
-
-struct ref {
-  struct spinlock lock;
-  int ref;
-};
-
-struct ref ref2pages[(PHYSTOP - KERNBASE) / PGSIZE];
 
 void
 kinit()
 {
-  for (int i = 0; i < (PHYSTOP - KERNBASE) / PGSIZE; i++)
-  {
-    initlock(&(ref2pages[i].lock), "page_ref");
-  }
   initlock(&kmem.lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
 void
-freerange(void* pa_start, void* pa_end)
+freerange(void *pa_start, void *pa_end)
 {
-  char* p;
+  char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for (; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-  {
-    ref2pages[((uint64)p - KERNBASE) / PGSIZE].ref = 1;
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
-  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -59,99 +44,39 @@ freerange(void* pa_start, void* pa_end)
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
-kfree(void* pa)
+kfree(void *pa)
 {
-  struct run* r;
+  struct run *r;
 
-  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-  {
-    printf("%p,%d\n", pa, krefget((void*)pa));
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
-  }
 
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
 
-  uint64 pg = ((uint64)pa - KERNBASE) / PGSIZE;
-  acquire(&ref2pages[pg].lock);
-  if (ref2pages[pg].ref == 0)
-  {
-    release(&ref2pages[pg].lock);
-    panic("kfree");
-  }
-  ref2pages[pg].ref--;
-  if (ref2pages[pg].ref != 0)
-  {
-    release(&ref2pages[pg].lock);
-    return;
-  }
-  else
-  {
-    // Fill with junk to catch dangling refs.
-    memset(pa, 1, PGSIZE);
+  r = (struct run*)pa;
 
-    r = (struct run*)pa;
-
-    acquire(&kmem.lock);
-    r->next = kmem.freelist;
-    kmem.freelist = r;
-    release(&kmem.lock);
-
-    release(&ref2pages[pg].lock);
-  }
-
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void*
+void *
 kalloc(void)
 {
-  struct run* r;
+  struct run *r;
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if (r)
-  {
-    uint64 pg = ((uint64)r - KERNBASE) / PGSIZE;
-    acquire(&ref2pages[pg].lock);
-    ref2pages[pg].ref = 1;
-    release(&ref2pages[pg].lock);
+  if(r)
     kmem.freelist = r->next;
-  }
-
   release(&kmem.lock);
 
-  if (r)
-  {
+  if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
-  }
-
   return (void*)r;
-}
-
-int krefget(void* pa)
-{
-  return ref2pages[((uint64)pa - KERNBASE) / PGSIZE].ref;
-}
-
-void
-krefadd(void* pa)
-{
-  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfrefadd");
-  uint64 pg = ((uint64)pa - KERNBASE) / PGSIZE;
-  acquire(&ref2pages[pg].lock);
-  ref2pages[pg].ref++;
-  release(&ref2pages[pg].lock);
-}
-
-void
-krefdec(void* pa)
-{
-  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfrefdec");
-  uint64 pg = ((uint64)pa - KERNBASE) / PGSIZE;
-  acquire(&ref2pages[pg].lock);
-  ref2pages[pg].ref--;
-  release(&ref2pages[pg].lock);
 }
